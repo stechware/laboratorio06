9beeae2d7f609aed12c823a38851c017b947002e
Juan Solar <stchware2023@gmail.com>
2025-02-17 08:11:21 -0500

    fuentes iniciales

---
+++ laboratorio06/src/main/java/com/idat/laboratorio06/Laboratorio06.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.idat.laboratorio06;
+
+/**
+ *
+ * @author IDAT
+ */
+public class Laboratorio06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
