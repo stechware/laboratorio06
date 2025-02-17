2d5e5ea2e6eee0022c18f21761f4a1dc3f5b9fdb
Juan Solar <stchware2023@gmail.com>
2025-02-17 10:23:36 -0500

    Creación de la Clase Cuadrado

---
+++ laboratorio06/src/main/java/reforzamiento/cuadrado/PruebaCuadrado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reforzamiento.cuadrado;
+
+/**
+ *
+ * @author IDAT
+ */
+public class PruebaCuadrado {
+    
+    public static void main(String[] args) {
+        
+        Cuadrado cuadrado = new Cuadrado(5);
+        cuadrado.imprimir();
+    }
+    
+    
+}
